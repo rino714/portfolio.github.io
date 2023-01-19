da2c3e9beb03085f1bb5d8b1902528f6ee2efe83
ItoRino <inono.i@icloud.com>
2023-01-19 15:51:19 +0900

    add:ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "users/:id" => "users#show"
 
   get "posts/index" => "posts#index"
-  get "posts/new" => "posts#new"
+  get "CPM-program/posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
   post "posts/create" => "posts#create"
   get "posts/:id/edit" => "posts#edit"
