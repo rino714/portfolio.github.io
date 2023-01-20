f27dc3abf9448d489e0e6cdc33bf5c01b4b527ff
ItoRino <inono.i@icloud.com>
2023-01-20 10:59:52 +0900

    add:ルート

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,5 @@ Rails.application.routes.draw do
   get "about" => "home#about"
 
   get "quiz/index" => "quiz#index"
-  get "CPM-program/quiz/quiz-2" => "quiz#quiz-2"
+  get "CPM-program/quiz/quiz-2" => "quiz#quiz-2.html"
 end
